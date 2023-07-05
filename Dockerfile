50bc35bcace805b9cfb6d034c22843096e4cccf9
Michael Rhee <michaelrhee@pm.me>
2023-07-05 16:19:51 -0500

    Fixing for staging

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM public.ecr.aws/lambda/nodejs:18
 # Copy function code and install dependencies
 RUN cd ${LAMBDA_TASK_ROOT}
 COPY index.js ${LAMBDA_TASK_ROOT}
+COPY db.js ${LAMBDA_TASK_ROOT}
 COPY package.json ${LAMBDA_TASK_ROOT}
 COPY package-lock.json ${LAMBDA_TASK_ROOT}
-COPY .env ${LAMBDA_TASK_ROOT}
+COPY .env.staging ${LAMBDA_TASK_ROOT}/.env
 RUN npm install --omit=dev
 
 # Set the CMD to your handler (could also be done as a parameter override outside of the Dockerfile)
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const config = require("dotenv").config();
 const AWS = require("aws-sdk");
-const fs = require("fs");
 const Readable = require("stream").Readable;
 const csv = require("csv-parser");
 const log = require("loglevel");
 log.setLevel(process.env.LOGLEVEL);
 const zlib = require("zlib");
+const db = require("./db");
 
 const s3 = new AWS.S3({
   apiVersion: "2006-03-01",
@@ -13,9 +13,6 @@ const s3 = new AWS.S3({
 });
 
 const s3BucketName = `${process.env.AWS_S3_BUCKET_NAME}`;
-const trackPrefix = `${process.env.AWS_S3_TRACK_PREFIX}`;
-const localConvertPath = `${process.env.LOCAL_CONVERT_PATH}`;
-const localUploadPath = `${process.env.LOCAL_UPLOAD_PATH}`;
 
 // Handler
 exports.handler = async function (event, context) {
@@ -53,62 +50,93 @@ exports.handler = async function (event, context) {
   const data = await getObject();
 
   log.debug("Decompressing file");
-  zlib.unzip(data.Body, (err, decompressed) => {
-    if (err) {
-      log.debug(`Error decompressing file: ${err}`);
-    }
-    console.log(decompressed);
-    // read the decompressed buffer to a readable stream
-    const buffer = Readable.from(decompressed);
-    buffer
-      .pipe(
-        csv({
-          skipLines: 2,
-          separator: "\t",
-          headers: [
-            "date",
-            "time",
-            "x-edge-location",
-            "sc-bytes",
-            "c-ip",
-            "cs-method",
-            "cs(Host)",
-            "cs-uri-stem",
-            "sc-status",
-            "cs(Referer)",
-            "cs(User-Agent)",
-            "cs-uri-query",
-            "cs(Cookie)",
-            "x-edge-result-type",
-            "x-edge-request-id",
-            "x-host-header",
-            "cs-protocol",
-            "cs-bytes",
-            "time-taken",
-            "x-forwarded-for",
-            "ssl-protocol",
-            "ssl-cipher",
-            "x-edge-response-result-type",
-            "cs-protocol-version",
-            "fle-status",
-            "fle-encrypted-fields",
-            "c-port",
-            "time-to-first-byte",
-            "x-edge-detailed-result-type",
-            "sc-content-type",
-            "sc-content-len",
-            "sc-range-start",
-            "sc-range-end",
-          ],
+
+  let inserts = [];
+  await new Promise((resolve, reject) => {
+    return zlib.unzip(data.Body, (err, decompressed) => {
+      if (err) {
+        log.debug(`Error decompressing file: ${err}`);
+      }
+
+      // read the decompressed buffer to a readable stream
+      const buffer = Readable.from(decompressed);
+      return buffer
+        .pipe(
+          csv({
+            skipLines: 2,
+            separator: "\t",
+            headers: headers, // Defined below
+          })
+        )
+        .on("data", (row) => {
+          // log.debug(row);
+          // log.debug(row["cs-uri-stem"]);
+          const uriStem = row["cs-uri-stem"];
+          const isTrack = row["cs-uri-stem"].match(/track/);
+          const trackKey = uriStem.match(
+            /[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/
+          );
+          log.debug(`${trackKey}`);
+          // TODO: Write row to database
+          if (isTrack) {
+            inserts.push({
+              track_id: `${trackKey}`,
+              user_id: row["cs(Referer)"],
+              created_at: `${row["date"]} ${row["time"]}`,
+              complete: true,
+            });
+          }
         })
-      )
-      .on("data", (row) => {
-        // log.debug(row);
-        log.debug(row["cs-uri-stem"]);
-        // TODO: Write row to database
-      })
-      .on("end", () => {
-        log.debug("CSV file successfully processed");
-      });
+        .on("end", () => {
+          return db
+            .knex("play")
+            .insert(inserts)
+            .then((result) => {
+              log.debug(result);
+              log.debug("Rows processed");
+              resolve();
+            })
+            .catch((err) => {
+              log.debug(err);
+              reject(err);
+            });
+        });
+    });
   });
 };
+
+const headers = [
+  "date",
+  "time",
+  "x-edge-location",
+  "sc-bytes",
+  "c-ip",
+  "cs-method",
+  "cs(Host)",
+  "cs-uri-stem",
+  "sc-status",
+  "cs(Referer)",
+  "cs(User-Agent)",
+  "cs-uri-query",
+  "cs(Cookie)",
+  "x-edge-result-type",
+  "x-edge-request-id",
+  "x-host-header",
+  "cs-protocol",
+  "cs-bytes",
+  "time-taken",
+  "x-forwarded-for",
+  "ssl-protocol",
+  "ssl-cipher",
+  "x-edge-response-result-type",
+  "cs-protocol-version",
+  "fle-status",
+  "fle-encrypted-fields",
+  "c-port",
+  "time-to-first-byte",
+  "x-edge-detailed-result-type",
+  "sc-content-type",
+  "sc-content-len",
+  "sc-range-start",
+  "sc-range-end",
+];
